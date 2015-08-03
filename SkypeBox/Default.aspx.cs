b101466b5957241affa7dcffa3fd117932c4be14
Régis Scyeur <regis.scyeur@zebrapuma.be>
2015-08-03 14:14:51 +0200

    Initial Commit

---
+++ SkypeBox/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+
+namespace SkypeBox
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("SkypeBox : " + DateTime.Now.ToString("dddd dd MMM yyyy hh:mm:ss.fff") + "<br/>");
+            Response.Write("MachineName : " + Environment.MachineName );
+
+
+            //IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            //foreach (var item in host.AddressList )
+            //{
+            //    Response.Write ( item.ToString() + "<br/>");
+            //}
+            //Debugger.Break();
+
+
+
+        }
+    }
+}
