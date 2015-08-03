6ee7e82d52605b4ebf8705653054d8e910cba936
Régis Scyeur <regis.scyeur@zebrapuma.be>
2015-08-03 18:26:07 +0200

    Detecting Platform and return different command based on it

--- MonoForm/Form1.cs
+++ MonoForm/Form1.cs
@@ -20,17 +20,52 @@ namespace MonoForm
 
         private void button1_Click(object sender, EventArgs e)
         {
-            String OSVersion =  System.Environment.OSVersion.ToString();
-            txtInfo.Text = OSVersion + "\r\n";
+            String Command = "";
+            String Arguments = "";
+            Platform Platform =  GetPlatform();
+            switch (Platform)
+            {
+                case Platform.Windows:
+                    Command = "ipconfig";
+                    break;
+                case Platform.Linux:
+                    Command = "ifconfig";
+                    break;
+                case Platform.Mac:
+                    Command = "ifconfig";
+                    break;
+                default:
+                    break;
+            }
+            txtInfo.Text = Platform + "\r\n" + "\r\n";
 
             Process pNet = new Process();
-            ProcessStartInfo psi = new ProcessStartInfo("ipconfig");
-            psi.Arguments = "";
+            ProcessStartInfo psi = new ProcessStartInfo(Command);
+            psi.Arguments = Arguments;
             psi.RedirectStandardOutput = true;
             psi.UseShellExecute = false;
             pNet.StartInfo = psi;
             pNet.Start();
             txtInfo.Text += pNet.StandardOutput.ReadToEnd();
+        }
+
+        private Platform GetPlatform() {
+            switch (System.Environment.OSVersion.Platform )
+            {
+                case PlatformID.MacOSX:
+                    return Platform.Mac;
+                case PlatformID.Unix:
+                    return Platform.Linux;
+                default:
+                    return Platform.Windows;
+            }
+        }
+
+        private enum Platform
+        {
+            Windows,
+            Linux,
+            Mac
         }
     }
 }
