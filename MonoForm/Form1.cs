c7563a9044f3bb5b65be771634e25193693e18ef
Régis Scyeur <regis.scyeur@zebrapuma.be>
2015-08-03 16:56:26 +0200

    lauch http://www.scyeur.net

--- MonoForm/Form1.cs
+++ MonoForm/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace MonoForm
 {
@@ -19,7 +20,12 @@ namespace MonoForm
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Process.Start("http://www.scyeur.net");
             MessageBox.Show("Hello World !");
+
+
+            
+
         }
     }
 }
